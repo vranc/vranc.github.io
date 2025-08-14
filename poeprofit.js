12d78872aed801dcb468a914bc9dcc7ecc49ec84
vranc <34917550+vranc@users.noreply.github.com>
2025-08-14 15:55:14 +0200

    Add files via upload

--- poeprofit.js
+++ poeprofit.js
@@ -1,1 +1,3 @@
-document.write(`hello world`);
+fetch("https://poe.ninja/api/data/currencyoverview?league=Mercenaries&type=Fragment")
+    .then(Response => console.log(Response))
+    .catch(error => console.error("Error fetching currency overview:", error))
