b71160600a75a4b1c59a9420a08ca3e981e77ecf
vranc <34917550+vranc@users.noreply.github.com>
2025-09-17 09:00:19 +0200

    Delete poeprofit.js

--- poeprofit.js
+++
@@ -1,3 +0,0 @@
-fetch("https://poe.ninja/api/data/currencyoverview?league=Mercenaries&type=Fragment")
-    .then(Response => console.log(Response))
-    .catch(error => console.error("Error fetching currency overview:", error))
